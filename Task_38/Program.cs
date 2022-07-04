d778d7af30dabe61b77ac203a3206188e69f32d3
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-07-04 09:54:58 +0300

    task 38 added

---
+++ Task_38/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.*/
+
+
+int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++) {
+    array[i] = new Random().Next(1,100);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+Array.Sort(array); // я отсортировала массив от наименьшего к наибольшему
+foreach (int i in array) {
+    Console.Write(i + " ");
+}
+Console.WriteLine();
+
+int min = array[0];
+int max = array.Last();
+Console.WriteLine(max-min);
+
+
